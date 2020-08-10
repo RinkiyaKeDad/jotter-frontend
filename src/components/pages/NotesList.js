c2cfd6ad8f374069255d8cfb097cf0a42510be1a
RinkiyaKeDad <arshsharma461@gmail.com>
2020-08-10 13:06:36 +0530

    deletion functionality added

--- src/components/pages/NotesList.js
+++ src/components/pages/NotesList.js
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Axios from 'axios';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { Link } from 'react-router-dom';
 
 import UserContext from '../context/UserContext';
+import { Button } from '@material-ui/core';
 
 export default function NotesList() {
   const { userData } = useContext(UserContext);
@@ -31,6 +33,24 @@ export default function NotesList() {
   }, [userData]);
 
   console.log(userData);
+
+  const deleteNote = async id => {
+    try {
+      const response = await Axios.delete(
+        process.env.REACT_APP_BACKEND_URL + `/notes/${id}`,
+        {
+          headers: {
+            'x-auth-token': userData.token,
+          },
+        }
+      );
+      const newNotes = notes.filter(note => note._id != id);
+      setNotes(newNotes);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       {notes &&
@@ -39,6 +59,9 @@ export default function NotesList() {
             <Link to={`/note/${note._id}/`} style={{ textDecoration: 'none' }}>
               {note.title}
             </Link>
+            <Button onClick={() => deleteNote(note._id)}>
+              <DeleteIcon />
+            </Button>
           </h1>
         ))}
     </div>
